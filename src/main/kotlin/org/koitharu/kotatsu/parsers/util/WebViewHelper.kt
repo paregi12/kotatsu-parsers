77c390ec16c7b36b0646aeed955e5dd42bf5e026
dragonx943 <premieregirl26@gmail.com>
2025-11-30 16:31:51 +0700

    chore(utils): Add getUrlValue function to WebViewHelper

--- src/main/kotlin/org/koitharu/kotatsu/parsers/util/WebViewHelper.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/util/WebViewHelper.kt
@@ -9,4 +9,8 @@ public class WebViewHelper(
 	public suspend fun getLocalStorageValue(domain: String, key: String): String? {
 		return context.evaluateJs("$SCHEME_HTTPS://$domain/", "window.localStorage.getItem(\"$key\")")
 	}
+
+    public suspend fun getUrlValue(url: String, value: String): String? {
+        return context.evaluateJs("$SCHEME_HTTPS://$url", "new URLSearchParams(window.location.search).get('$value');")
+    }
 }
