c325639dede87886c265e53d068ae1a70a80e820
dragonx943 <premieregirl26@gmail.com>
2025-12-27 16:43:51 +0700

    Ikiru: Allow to change User-Agent in source settings

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/natsu/id/Ikiru.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/natsu/id/Ikiru.kt
@@ -9,5 +9,11 @@ import org.koitharu.kotatsu.parsers.site.natsu.NatsuParser
 @MangaSourceParser("IKIRU", "Ikiru", "id")
 internal class Ikiru(context: MangaLoaderContext) :
     NatsuParser(context, MangaParserSource.IKIRU, pageSize = 24) {
+
     override val configKeyDomain = ConfigKey.Domain("02.ikiru.wtf")
+
+	override fun onCreateConfig(keys: MutableCollection<ConfigKey<*>>) {
+		super.onCreateConfig(keys)
+		keys.add(userAgentKey)
+	}
 }
