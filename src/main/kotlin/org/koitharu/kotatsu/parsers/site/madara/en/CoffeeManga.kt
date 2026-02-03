928864afb06c506277df50cd5190e247f56a77c6
AgentKush <144564801+AgentKush@users.noreply.github.com>
2026-02-03 11:13:36 +0000

    fix(CoffeeManga): Update domain to coffeemanga.ink
    Close #40

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/CoffeeManga.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/CoffeeManga.kt
@@ -7,4 +7,4 @@ import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("COFFEE_MANGA", "CoffeeManga", "en")
 internal class CoffeeManga(context: MangaLoaderContext) :
-	MadaraParser(context, MangaParserSource.COFFEE_MANGA, "coffeemanga.io")
+	MadaraParser(context, MangaParserSource.COFFEE_MANGA, "coffeemanga.ink")
